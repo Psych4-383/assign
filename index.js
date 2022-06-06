9a5292798169ad371cf2c0b799ba397d65244fc2
Miss Bisht <73991231+Psych4-383@users.noreply.github.com>
2022-06-06 11:56:10 +0530

    added heroku

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express")
 
 var app = express()
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 app.get("/", function (request, response) {
     response.send("Hello World!")
 })
 
-app.listen(10000, function () {
-    console.log("Started application on port %d", 10000)
+app.listen(port, function () {
+    console.log("Started application on port %d", port)
 });
